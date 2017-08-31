3ebc03fb55be96c5c74c5d11192ffdba8e706a89
Siddharth Mittal <smittal6@gmail.com>
2017-09-01 03:09:41 +0530

    implement Yield

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -207,7 +207,15 @@ ExceptionHandler(ExceptionType which)
        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
-	}else
+	}
+	else if((which == SyscallException) && (type == SysCall_Yield)){
+       currentThread->YieldCPU();
+       // Advance program counters.
+       machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+       machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+       machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+    }
+	else
 	{
 	printf("Unexpected user mode exception %d %d\n", which, type);
 	ASSERT(FALSE);
